d4d680518dc579e89796aa672ca11b5af082d7fa
cschneuw <christelle.schneuwly@epfl.ch>
2019-10-07 20:49:12 +0200

    Create implementations.py
    implemetations.py file created

---
+++ scripts/implementations.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+""" Implementation of Machine Learning functions. """
+
+def least_squares(y, tx):
+    """ Linear regression using gradient descent. """
+    return (w, loss)
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """ Linear regression using stochastic gradient descent. """
+    return (w, loss)
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """ Least squares regression using normal equations. """
+    return (w, loss)
+
+def ridge_regression(y, tx, lambda_):
+    """ Ridge regression using normal equations. """
+    return (w, loss)
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """ Logistic regression using gradient descent or SGD. """
+    return (w, loss)
+
+def reg_logistic_regression(y, tx, lambda_, initial w, max_iters, gamma):
+    """ Regularized logistic regression using gradient descent or SGD. """
+    return (w, loss)
+    
