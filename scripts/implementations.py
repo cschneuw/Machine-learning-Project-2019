27d2dda2f8f74dcfc2aa7bc8ec47be73bd1c6477
cschneuw <christelle.schneuwly@epfl.ch>
2019-10-17 14:46:37 +0200

    trials on logistic regression

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -84,7 +84,7 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     
     w = initial_w
     for n_iter in range(max_iters):
-        g, e = compute_gradient(y, tx, w)
+        g = compute_gradient(y, tx, w)
         loss = compute_mse(y, tx, w)
         w = w - gamma * g
         
@@ -97,7 +97,7 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for n_iter in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
-            g, e = compute_gradient(y_batch, tx_batch, w)
+            g = compute_gradient(y_batch, tx_batch, w)
             w = w - gamma * g
             loss = compute_mse(y, tx, w)
             
@@ -119,10 +119,9 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     
     w = initial_w
     for n_iter in range(max_iters):
-        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
-            g = calculate_log_gradient(y, tx, w)
-            
-            loss = calculate_loglikelihood(y, tx, w)
+        g = compute_log_gradient(y, tx, w)
+        w = w - gamma * g
+        loss = compute_loglikelihood(y, tx, w)
             
     return (w, loss)
 
@@ -133,10 +132,9 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     N = len(y)
     w = initial_w
     for n_iter in range(max_iters):
-        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
-            g = calculate_gradient(y, tx, w)+lambda_*w/N
-            w = w - gamma * g
-            loss = calculate_loglikelihood(y, tx, w)+lambda_*np.sum(w**2)/(2*N)
+        g = compute_log_gradient(y, tx, w)+lambda_*w/N
+        w = w - gamma * g
+        loss = compute_loglikelihood(y, tx, w)+lambda_*np.sum(w**2)/(2*N)
             
     return (w, loss)
 
@@ -204,19 +202,114 @@ def cross_validation(y, x, k_indices, k, lambda_, degree):
     
     return np.mean(np.array(loss_tr)), np.mean(np.array(loss_te))
 
+def cross_validation_log(y, x, k_indices, k, lambda_, degree):
+    """return the loss of ridge regression."""
+
+    te_indices = k_indices[k]
+    tr_indices = [ind for split in k_indices for ind in split if ind not in te_indices] 
+    x_tr = x[tr_indices, :]
+    x_te = x[te_indices, :]
+    y_tr = y[tr_indices]
+    y_te = y[te_indices]
+        
+    tx_tr = build_poly(x_tr, degree)
+    tx_te = build_poly(x_te, degree)
+        
+    w_tr, loss_tr = reg_logistic_regression(y_tr, tx_tr, lambda_)
+        
+    loss_te = compute_loglikelihood(y_te, tx_te, w_tr)
+    
+    return np.mean(np.array(loss_tr)), np.mean(np.array(loss_te))
+
 # %% Addtional methods
 
-def transform_factor(x, column_idx, nlevels):
+def separate_factor(x, nlevels=4, column_idx = 22):
     """ Transform a column with categorical variables into different columns with binary data.
     E.g feature = [1, 2, 1, 3] -> features = [[1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]  """
-   
+    
     new_var = np.zeros((x.shape[0], nlevels))
+
     for i in range(x.shape[0]):
         for j in range(nlevels):
             if x[i,column_idx] == j:
                 new_var[j,0] = 1
-               
+
     x = np.delete(x, column_idx, axis = 1)
-   
-    return np.concatenate((x, new_var), axis=1)
+
+    return x, new_var
+
+
+def build_interaction(x):
+    """ Build a matrix containing the interaction terms of the input features e.g. x1 * x2, x1 * x3, etc """
+
+    comb = np.ones((x.shape[0],)).reshape(-1,1)
+
+    for i in range(x.shape[1]-1):
+        for j in range(i+1,x.shape[1]):
+            temp = x[:,i] * x[:,j]
+            comb = np.concatenate((comb, temp.reshape(-1,1)), axis=1)
+            
+    return np.delete(comb, 0, axis = 1)
+
+
+def missingness_filter(cX, cutoff = 0.5):
+    """ Removes all features with more than the missingness cutoff """
+
+    cX = np.where(cX == -999, np.nan, cX)
+    missingness = np.sum(np.isnan(cX), axis = 0)/cX.shape[0]
+
+    to_remove = np.where(missingness > cutoff)[0]
+
+    return np.delete(cX, to_remove, axis = 1), to_remove
+
+
+def impute_mean(x):
     
+    out = np.zeros(x.shape)
+    for i in range(x.shape[1]):
+        temp = x[:,i]
+        mean = np.nanmean(temp)
+        out[:,i] = np.nan_to_num(temp, nan = mean)
+
+    return out
+
+
+def impute_median(x):
+    
+    out = np.zeros(x.shape)
+    for i in range(x.shape[1]):
+        temp = x[:,i]
+        median = np.nanmedian(temp)
+        out[:,i] = np.nan_to_num(temp, nan = median)
+
+    return out
+    
+    
+def impute_gaussian(x):
+    out = np.zeros(x.shape)
+    for i in range(x.shape[1]):
+        temp = x[:,i]
+        mean = np.nanmean(temp)
+        std = np.nanstd(temp)
+
+        for j in range(x.shape[0]):
+            out[j,i] = np.nan_to_num(temp[j], nan = np.random.normal(loc=mean, scale=std))
+
+    return out
+
+def train_data_formatting(tX, degree = 2, cutoff = 0.6, imputation = impute_mean, interaction = False):
+
+    #separating out the categorical variables
+    cont_X, fac_X = separate_factor(tX)
+    #applying a missingness filter on the columns/features
+    cont_X, to_remove = missingness_filter(cont_X, cutoff)
+    #imputing the missing data
+    cont_X = imputation(cont_X)
+    poly = build_poly(cont_X, degree)
+    poly = np.concatenate((poly, fac_X), axis=1)
+    
+    if lin_comb:
+        inter = build_interaction(cont_X)
+        return np.concatenate((poly, inter), axis=1), to_remove
+
+    return poly, to_remove
